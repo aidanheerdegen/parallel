07cf92f1b614d5b18453eb69c7025b767ae45130
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2017-02-09 10:49:31 +1100

    Compile modules separately

--- makefile
+++ makefile
@@ -3,10 +3,12 @@ c:
 
 f:
 	ifort -O0 -o timeme timeme.f90 timer_module.f90 $(LIBS)
-	ifort -O0 -openmp -o timeme_openmp timeme_openmp.f90 timer_module.f90 $(LIBS)
+	ifort -c timer_module.f90 $(LIBS)
+	ifort -O0 -openmp -o timeme_openmp timeme_openmp.f90 timer_module.o $(LIBS)
 	mpif90 -O0 -o timeme_mpi timeme_mpi.f90 $(LIBS)
 	mpif90 -O0 -o mpiexample mpiexample.f90
-	ifort -O0 -o timeme_papi timeme_papi.f90 papitime.f90  -I/apps/papi/5.4.3/include/ -lpapi
+	ifort -c papitime.f90  $(LIBS) -I/apps/papi/5.4.3/include/
+	ifort -O0 -o timeme_papi timeme_papi.f90 papitime.o  -I/apps/papi/5.4.3/include/ -lpapi
 
 clean:
 	rm -f *.o
---
+++ timeme.f90
@@ -0,0 +1,44 @@
+program multiply_matrices
+
+  use timer
+
+  implicit none
+
+  real(kind=8), allocatable :: A(:, :)
+  real(kind=8), allocatable :: B(:, :)
+  real(kind=8), allocatable :: C(:, :)
+
+  integer :: i, row, col
+
+  real :: elapsed
+
+  ! Allocate memory.
+  allocate(A(1000, 1000))
+  allocate(B(1000, 1000))
+  allocate(C(1000, 1000))
+
+  ! Fill A and B with random numbers.
+  call random_number(A)
+  call random_number(B)
+
+  ! Initialise C to zero.
+  C = 0.0
+
+  call start_timer()
+  do i = 1, 1000
+     print *,i,elapsed
+     do row = 1, 1000
+        do col = 1, 1000
+           C(col, row) = C(col, row) + A(col, i) * B(i, row)
+        end do
+     end do
+  end do
+  elapsed = lap_time()
+  write(*, *) "Call to calculate took",elapsed," seconds."
+
+  ! Free memory.
+  deallocate(C)
+  deallocate(B)
+  deallocate(A)
+
+end program multiply_matrices
